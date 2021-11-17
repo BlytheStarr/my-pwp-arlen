855fd19a4bd592b6a9d48a3bbbb0ee03e7876e8d
Arlen Smith <Asmith213@cnm.edu>
2021-11-17 03:43:09 -0700

    cleaned up unused code and added some annotations

--- public_html/script.js
+++ public_html/script.js
@@ -52,18 +52,3 @@ $(document).ready(function(){
     })
 })
 // Contact Form
-
-// // Horizontal Accordion
-// const horizontalAccordions = $(".accordion.width");
-//
-// horizontalAccordions.each((index, element) => {
-//     const accordion = $(element);
-//     const collapse = accordion.find(".collapse");
-//     const bodies = collapse.find("> *");
-//     accordion.height(accordion.height());
-//     bodies.width(bodies.eq(0).width());
-//     collapse.not(".show").each((index, element) => {
-//         $(element).parent().find("[data-toggle='collapse']").addClass("collapsed");
-//     });
-// });
-// // Horizontal Accordion
